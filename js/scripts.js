b48776c93d4e42956432dd05c172ac50b366cb1d
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 16:16:40 -0700

    attempting to store values from forms in variables created outside stressTest.submit function, code not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
+
+
+
   $('#stressTest').submit(function(event){
 
     $('input:checkbox[name=stressSigns]:checked').each(function(){
-
+      var section1answers = []; 
+      sections1answers = section1answers.push($(this).val());
+      console.log(section1answers);
     });
 
     $('input:checkbox[name=symptoms]:checked').each(function(){
-
+      var section2answers = $(this).val();
+      
     });
 
     $('input:checkbox[name=copingMethods]:checked').each(function(){
-
+      var section3answers = $(this).val();
+    
     });
-
-
-
-
-
+    
     $('#recommendations').show();
     event.preventDefault();
   });
