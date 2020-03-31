c79fee2f39eade04a6e0e765dc2500ac6add68f0
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 15:51:53 -0700

    add js returning values from html checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-  
+  $('#stressTest').submit(function(event){
+
+    $('input:checkbox[name=stressSigns]:checked').each(function(){
+
+    });
+
+    $('input:checkbox[name=symptoms]:checked').each(function(){
+
+    });
+
+    $('input:checkbox[name=copingMethods]:checked').each(function(){
+
+    });
+
+
+
+
+
+    $('#recommendations').show();
+    event.preventDefault();
+  });
 });
